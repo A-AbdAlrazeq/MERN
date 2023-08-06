0de251bd35eda3b46cebc87229001a776fbb66a9
A-AbdAlrazeq <107461563+A-AbdAlrazeq@users.noreply.github.com>
2023-08-06 17:13:38 +0300

    create a birthday button that will increase one person's age by one

--- React/Prop_It _Up/person_card/src/components/PersonCard.js
+++ React/Prop_It _Up/person_card/src/components/PersonCard.js
@@ -1,13 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 const PersonCard = (props) => {
+  const [age, setAge] = useState(props.age);
   return (
     <div>
       <h1>
         My name is {props.firstName},{props.lastName}
       </h1>
-      <p>Age: {props.age}</p>
+      <p>Age: {age}</p>
       <p>Hair Color: {props.hairColor}</p>
+      <button onClick={() => setAge(age + 1)}>
+        Birthday Button for {props.firstName} {props.lastName}
+      </button>
     </div>
   );
 };
