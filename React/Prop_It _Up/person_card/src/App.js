0d3d2e0a08beda78af02b4a897ad30d5b9532f44
A-AbdAlrazeq <107461563+A-AbdAlrazeq@users.noreply.github.com>
2023-08-07 17:34:07 +0300

    Box Generator

--- React/Prop_It _Up/person_card/src/App.js
+++ React/Prop_It _Up/person_card/src/App.js
@@ -1,9 +1,16 @@
 /* import PersonCard from "./components/PersonCard"; */
 import "./App.css";
-import Form from "./components/form";
+import React, { useState } from "react";
+/* import Form from "./components/form"; */
+import BoxGenerator from "./components/boxGenerator";
 import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
+  const [boxes, setBoxes] = useState([]);
+
+  const handleAddBox = (newBox) => {
+    setBoxes([...boxes, newBox]);
+  };
   return (
     /*  <div className="App">
       <PersonCard
@@ -26,7 +33,21 @@ function App() {
       />
     </div> */
     <div className="App">
-      <Form />
+      {/* <Form /> */}
+      <BoxGenerator onAddBox={handleAddBox} />
+      <div className="box">
+        {boxes.map((box, index) => (
+          <div
+            key={index}
+            style={{
+              backgroundColor: box.color,
+              width: box.width,
+              height: box.height,
+              margin: "5px",
+            }}
+          />
+        ))}
+      </div>
     </div>
   );
 }
