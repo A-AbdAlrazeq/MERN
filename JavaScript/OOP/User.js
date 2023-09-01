d9d90fbe1f97b8e07acac4e564358244aabb5c36
A-AbdAlrazeq <107461563+A-AbdAlrazeq@users.noreply.github.com>
2023-09-01 16:18:07 +0300

    add chain to method

--- JavaScript/OOP/User.js
+++ JavaScript/OOP/User.js
@@ -5,6 +5,7 @@ class User {
   }
   makeDeposit(amount) {
     this.accountBalance += amount;
+    return this;
   }
   makeWithdrawal(amount) {
     if (amount <= this.accountBalance) {
@@ -12,9 +13,11 @@ class User {
     } else {
       console.log("Insufficient funds.");
     }
+    return this;
   }
   displayBalance() {
     console.log(`User: ${this.name}, Balance: $${this.accountBalance}`);
+    return this;
   }
   transferMoney(otherUser, amount) {
     if (amount <= this.accountBalance) {
@@ -24,6 +27,7 @@ class User {
     } else {
       console.log("Insufficient funds for transfer.");
     }
+    return this;
   }
 }
 
@@ -32,23 +36,26 @@ const user2 = new User("Ahmad");
 const user3 = new User("Rami");
 
 //Have the first user make 3 deposits and 1 withdrawal and then display their balance
-user1.makeDeposit(100);
-user1.makeDeposit(150);
-user1.makeDeposit(70);
-user1.makeWithdrawal(200);
-user1.displayBalance();
+user1
+  .makeDeposit(100)
+  .makeDeposit(150)
+  .makeDeposit(70)
+  .makeWithdrawal(200)
+  .displayBalance();
 //Have the second user make 2 deposits and 2 withdrawals and then display their balance
-user2.makeDeposit(100);
-user2.makeDeposit(150);
-user2.makeWithdrawal(50);
-user2.makeWithdrawal(200);
-user2.displayBalance();
+user2
+  .makeDeposit(100)
+  .makeDeposit(150)
+  .makeWithdrawal(50)
+  .makeWithdrawal(200)
+  .displayBalance();
 //Have the third user make 1 deposits and 3 withdrawals and then display their balance
-user3.makeDeposit(300);
-user3.makeWithdrawal(150);
-user3.makeWithdrawal(50);
-user3.makeWithdrawal(200);
-user3.displayBalance();
+user3
+  .makeDeposit(300)
+  .makeWithdrawal(150)
+  .makeWithdrawal(50)
+  .makeWithdrawal(200)
+  .displayBalance();
 // Bonus: Transfer money from User 1 to User 3
 user1.transferMoney(user3, 50);
 // Display balances after the transfer
