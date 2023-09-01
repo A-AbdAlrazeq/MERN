756d1c14701e21c7aa4aad7d43714b7861e24fcf
A-AbdAlrazeq <107461563+A-AbdAlrazeq@users.noreply.github.com>
2023-09-02 00:22:39 +0300

    Super Ninja

---
+++ JavaScript/OOP/Super Ninja.js
@@ -0,0 +1,40 @@
+class Ninja {
+  constructor(name, health, speed = 3, strength = 3) {
+    this.name = name;
+    this.health = health;
+    this.speed = speed;
+    this.strength = strength;
+  }
+  sayName() {
+    console.log(`Ninja Name: ${this.name}`);
+    return this;
+  }
+  showStats() {
+    console.log(
+      `Ninja Name: ${this.name},Health:${this.health} Speed:${this.speed} Strength:${this.strength}`
+    );
+    return this;
+  }
+  drinkSake() {
+    this.health += 10;
+    return this;
+  }
+}
+class Sensei extends Ninja {
+  constructor(name, wisdom = 10) {
+    super(name, 200, 10, 10);
+    this.name = name;
+    this.wisdom = wisdom;
+  }
+  speakWisdom() {
+    super.drinkSake();
+    console.log(
+      "What one programmer can do in one month, two programmers can do in two months."
+    );
+  }
+}
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom();
+// -> "What one programmer can do in one month, two programmers can do in two months."
+superSensei.showStats();
+// -> "Name: Master Splinter, Health: 210, Speed: 10, Strength: 10"
