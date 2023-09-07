8a48ec31fb6b08bfa77b9dee8d882638a44ebeca
A-AbdAlrazeq <107461563+A-AbdAlrazeq@users.noreply.github.com>
2023-09-07 23:45:41 +0300

    add userId to token and store user data in request

--- FullStack MERN/blogify/server/middleware/isLogin.js
+++ FullStack MERN/blogify/server/middleware/isLogin.js
@@ -1,10 +1,24 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/User/User");
 const isLogin = (req, res, next) => {
-  console.log("is Login Middleware");
   //Get token from header
+  const token = req.headers.authorization?.split(" ")[1];
+  console.log(token);
   //? Verify the token
-  //get the user id
-  //save user into req obj
-  next(); //to execute getProfile in router
+  jwt.verify(token, "anykey", async (err, decoded) => {
+    //add user to req obj
+    //get the user id
+    const userId = decoded?.user?.id;
+    const user = await User.findById(userId).select("username email role _id");
+    //save user into req obj
+    req.userAuth = user;
+    if (err) {
+      return "Invalid token";
+    } else {
+      //!save the user
+      //*send the user
+      next();
+    }
+  });
 };
-
 module.exports = isLogin;
