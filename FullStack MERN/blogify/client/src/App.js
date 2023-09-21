0a6e3f7f16700b87ac53c245a936f0d6a204b375
A-AbdAlrazeq <107461563+A-AbdAlrazeq@users.noreply.github.com>
2023-09-21 23:57:53 +0300

    private and public navbar

--- FullStack MERN/blogify/client/src/App.js
+++ FullStack MERN/blogify/client/src/App.js
@@ -1,13 +1,23 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useSelector } from "react-redux";
 import Homepage from "./components/Homepage/Homepage";
 import Login from "./components/Users/Login";
 import UserProfile from "./components/Users/UserProfile";
+import PublicNavbar from "./components/Navbar/PublicNavbar";
+import Register from "./components/Users/Register";
+import PrivateNavbar from "./components/Navbar/PrivateNavbar";
 export default function App() {
+  //! Get the login user from store
+  const { userAuth } = useSelector((state) => state?.users);
+  const isLogin = userAuth?.userInfo?.token;
   return (
     <BrowserRouter>
+      {/* Navbar here */}
+      {isLogin ? <PrivateNavbar /> : <PublicNavbar />}
       <Routes>
         <Route path="/" element={<Homepage />}></Route>
         <Route path="/login" element={<Login />}></Route>
+        <Route path="/register" element={<Register />}></Route>
         <Route path="/profile" element={<UserProfile />}></Route>
       </Routes>
     </BrowserRouter>
